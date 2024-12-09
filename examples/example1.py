80bb7bd65a0b74e474a484510895c776e4444cd8
Ronald <hpxu1314@gmail.com>
2024-12-09 19:14:26 +0800

    Update example1.py and add all running results

--- examples/example1.py
+++ examples/example1.py
@@ -16,7 +16,7 @@ contour = np.squeeze(contours[0])
 # forward elliptic Fourier
 efDescriptor = EllipticFourier()
 
-A0, C0, coeffs = efDescriptor.forward(contour=contour, N=100)
+A0, C0, coeffs = efDescriptor.forward(contour=contour, N=10)
 print(f"A0: {A0}")
 print(f"C0: {C0}")
 print("Coefficients:")
